1c66dcf2f44b7e39e4ef9ec772b1b5c2c8263e49
yarec <yarec@163.com>
2015-05-14 19:40:59 +0800

    u

---
+++ MBProgresHUD/MBProgresHUD.h
@@ -0,0 +1,19 @@
+//
+//  MBProgresHUD.h
+//  MBProgresHUD
+//
+//  Created by r.t on 15/5/14.
+//  Copyright (c) 2015年 Matej Bukovinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MBProgresHUD.
+FOUNDATION_EXPORT double MBProgresHUDVersionNumber;
+
+//! Project version string for MBProgresHUD.
+FOUNDATION_EXPORT const unsigned char MBProgresHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBProgresHUD/PublicHeader.h>
+
+
